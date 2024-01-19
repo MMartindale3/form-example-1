0d64b6214055c46e8177785d2bd145090124881d
Melissa Martindale <mmartindale3@nait.ca>
2024-01-18 20:44:22 -0700

    notes index.js. connected tax-form.html & index.js

---
+++ js/index.js
@@ -0,0 +1,23 @@
+/* 
+    FUNCTIONS
+    reusable action use case
+
+    Terminology
+    function body
+    function addNumbers() {
+        js code runs
+    }
+
+    calling a function / invoking a function
+    nameFn()
+    addNumbers(arg, arg, arg)
+    addEventListener()
+*/
+
+// arg(2)      num1 = 2
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+// fn() call/ invoking
+const sum = addNumbers(2, 3); /* for some reason mine doesn't show up */
